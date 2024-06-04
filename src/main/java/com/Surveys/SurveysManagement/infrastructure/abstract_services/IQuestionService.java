9361bf007b6db46189652eebe899336becf9cd9b
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-06-04 11:31:56 -0500

    Question Microservice update

---
+++ src/main/java/com/Surveys/SurveysManagement/infrastructure/abstract_services/IQuestionService.java
@@ -0,0 +1,8 @@
+package com.Surveys.SurveysManagement.infrastructure.abstract_services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IQuestionService extends CrudService<Long, Long, Long> {
+
+}
